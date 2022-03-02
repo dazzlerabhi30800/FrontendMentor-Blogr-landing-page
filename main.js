2aed0084bda5b87a7f1bd46441d9c6df7e05c869
dazzlerabhi30800 <abhisingh30800@gmail.com>
2022-03-02 18:09:24 +0530

    Refix code 4

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const menuBtn = document.querySelector('.menu-btn i');
 const navbar = document.querySelector('.navbar');
-// const connect = document.querySelector('.connect-wrapper');
-// const texts = document.querySelectorAll('.connect-wrapper ul li');
 const product = document.querySelector('.product i');
 const detailOne = document.querySelector('.one');
 const company = document.querySelector('.company i')
@@ -13,7 +11,6 @@ const detailWrapperOne = document.querySelector('#first');
 const detailWrapperTwo = document.querySelector('#second');
 const detailWrapperThree = document.querySelector('#third');
 
-console.log(detailOne);
 
 menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('fa-times');
@@ -41,30 +38,12 @@ connect.addEventListener('click', () => {
     detailWrapperThree.classList.toggle('active');
 })
 
-// productLink.addEventListener('mouseover', () => {
-//     product.classList.add('fa-chevron-up');
-//     detailOne.classList.toggle('active');
-// });
-// productLink.addEventListener('mouseleave', () => {
-//     product.classList.remove('fa-chevron-up');
-//     detailOne.classList.remove('active');
-// });
 
-
-
-// arrow.addEventListener('click', () => {
-//     console.log('clicked!');
-//     arrow.classList.toggle('fa-chevron-up');
-//     connect.classList.toggle('active');
-//     texts.forEach(text => (
-//         text.classList.toggle('active')
-//     ))
-// })
 
 
 const secondImg = document.querySelector('.second .future-wrapper .img-wrapper img');
 const fourthImg = document.querySelector('.fourth .future-wrapper .img-wrapper img');
-console.log(fourthImg);
+
 
 if(window.innerWidth >= 1000) {
     secondImg.src = "images/illustration-editor-desktop.svg"
